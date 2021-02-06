68f56b7596a31982189e5d9d95d027ed3fc721df
Lucas Senna <lucasenna95@gmail.com>
2021-02-06 19:25:25 -0300

    Adição arquivo JQuery e chamada do Jquery no projeto

--- layout/menu.php
+++ layout/menu.php
@@ -2,7 +2,7 @@
     <div id="sidebar">
         <div class="sidebar-wrapper active ps ps--active-y">
             <div class="m-4">
-                <a href="http://localhost:8082/index.php">
+                <a href="index.php">
                     <img class="img-fluid" src="http://ava.egp.ce.gov.br/2018/desv/logo/egp.png" alt="Logo EGPCE" srcset="">
                 </a>
             </div>
@@ -21,25 +21,25 @@
                         </a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="../Topico01.php" class="sidebar-link">
+                        <a href="/Topico01.php" class="sidebar-link">
                             <i data-feather="home" width="40"></i>
                             <span>1.0 - Panorama do Comportamento Digital</span>
                         </a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="../Topico02.php" class="sidebar-link">
+                        <a href="/Topico02.php" class="sidebar-link">
                             <i data-feather="home" width="60"></i>
                             <span>2.0 - Eventos Presenciais, Híbridos e Digitais como Ferramenta de Educação</span>
                         </a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="../Topico03.php" class="sidebar-link">
+                        <a href="/Topico03.php" class="sidebar-link">
                             <i data-feather="home" width="40"></i>
                             <span>3.0 - Design e Planejamento de Eventos Digitais</span>
                         </a>
                     </li>
                     <li class="sidebar-item">
-                        <a href="../Topico04.php" class="sidebar-link">
+                        <a href="/Topico04.php" class="sidebar-link">
                             <i data-feather="home" width="20"></i>
                             <span>4.0 - Tipologia de Eventos Digitais</span>
                         </a>
